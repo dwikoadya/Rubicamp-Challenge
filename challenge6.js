d451f4e4ca6f34a4e63db106a6521305ac33b86c
dwikoadya <dwikoadya@gmail.com>
2020-06-20 22:38:24 +0700

    Challenge 6

---
+++ challenge6.js
@@ -0,0 +1,21 @@
+function sentencesManipulation(sentence) {
+  var z = sentence.split(" ");
+  var str = '';
+    for (let i = 0; i < z.length; i++) {
+      str += stringManipulation(z[i]) + ' ';
+    }
+    console.log(str);
+}
+
+
+
+function stringManipulation(word) {
+  huruf = word.charAt(0);
+  if (huruf === 'a' || huruf === 'i' || huruf === 'u' || huruf === 'e' || huruf ==='o') {
+    return word
+  } else {
+    return word.slice(1) + word.slice(0,1) + 'nyo';
+  }
+}
+
+sentencesManipulation('ibu pergi ke pasar bersama aku');
